950cc407f280ab4118d31f590649fcc77bcddfd7
Adithiya <adithiya1805@gmail.com>
2022-05-13 17:41:34 +0530

    Initial Commit: I made this basic Calculator program using Java

---
+++ Calculator.java
@@ -0,0 +1,48 @@
+public class Calculator {
+    import java.util.Scanner ;
+
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+
+        int ans = 0;
+
+        while (true) {
+            System.out.print("Enter the operator: ");
+            char op = in.next().trim().charAt(0);
+
+            if (op == '+' || op == '-' || op == '*' || op == '/' || op == '%') {
+                System.out.print("Enter two numbers: ");
+
+                int num1 = in.nextInt();
+                int num2 = in.nextInt();
+
+                if (op == '+') {
+                    ans = num1 + num2;
+                }
+                if (op == '-') {
+                    ans = num1 - num2;
+
+                }
+                if (op == '*') {
+                    ans = num1 * num2;
+                }
+                if (op == '/') {
+                    ans = num1 / num2;
+                }
+                if (op == '%') {
+                    ans = num1 % num2;
+                }
+            } else if (op == 'x' || op == 'X') {
+                break;
+            } else {
+                System.out.println("Invalid operator: ");
+            }
+
+            System.out.println(ans);
+        }
+    }
+
+}
+
+
+}
